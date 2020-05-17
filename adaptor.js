bda45f8a21e2a34f44d04e85828451ff62cb86d7
Duong Dinh Cuong <cuong3ihut@gmail.com>
2020-05-17 15:54:16 +0200

    Update test-units and test-apis

--- adaptor.js
+++ adaptor.js
@@ -57,7 +57,7 @@ function getProperties(obj, define, prefix) {
                 const varData = (obj[key] || '').toCamelCase().split(' ').join('').split('-').join('')
                 properties[varName] = varData
                 properties[varName + 'Title'] = (obj[key] || '').split('-').join(' ')
-                properties[varName + 'Posix'] = Case.snake(varData).split('_').join('-');
+                properties[varName + 'Snake'] = Case.snake(varData).split('_').join('-');
                 properties[varName + 'Pascal'] = varData.toPascalCase()
                 properties[varName + 'Origin'] = obj[key]
                 properties[varName + obj[key].toPascalCase()] = true
@@ -582,6 +582,10 @@ function removeCustomPrefix(obj) {
     return tmp
 }
 
+function cryptoRandomApiKey(size) {
+    return crypto.randomBytes(size).toString('hex').slice(0, size)
+}
+
 function cryptoRandomNumber(minimum, maximum){
 	var distance = maximum-minimum;
 	if(minimum>=maximum){
@@ -620,7 +624,7 @@ function getPrime(api, defaults) {
     prime.quotaLimit = prime.quotaLimit || 100
     prime.projectName = api['x-project-name'].toCamelCase().split(' ').join('').split('-').join('');
     prime.projectNamePascal = prime.projectName.toPascalCase().split(' ').join('').split('-').join('');
-    prime.projectNamePosix = Case.snake(prime.projectName).split('_').join('-');
+    prime.projectNameSnake = Case.snake(prime.projectName).split('_').join('-');
     prime.appDescription = api.info.description || api.info.title || 'Missing description in info.title or info.description';
     prime.projectDescription = prime.appDescription;
     prime.serviceDescription = prime.appDescription;
@@ -630,6 +634,8 @@ function getPrime(api, defaults) {
     prime.projectVersion = "0.1.1";
     prime.projectId = api['x-project-id'] || cryptoRandomNumber(10000000, 99999999)
     api['x-project-id'] = prime.projectId
+    prime.gatewayApiKey = api['x-api-key'] || cryptoRandomApiKey(40)
+    api['x-api-key'] = prime.gatewayApiKey
     prime.version = api.info.version;
     prime.title = api.info.title;
     prime.generatorVersion = require('./package.json').version;
